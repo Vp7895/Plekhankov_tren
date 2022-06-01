919d12a5b1754d64f79182f277155a9759d5c4f6
Vanya984 <plekhankov68@gmail.com>
2022-06-01 17:31:07 +0300

    desktop
    Реализовал переходы (2 страницы), вывод двух таблиц

---
+++ tren1_Plekhankov/tren1_Plekhankov/Page/MaterialsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using tren1_Plekhankov.DateBase;
+
+namespace tren1_Plekhankov.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для MaterialsPage.xaml
+    /// </summary>
+    public partial class MaterialsPage : Window
+    {
+        public MaterialsPage()
+        {
+            InitializeComponent();
+
+            DGridMaterials.ItemsSource = tren1_PlekhankovEntities.GetContext().Materials.ToList();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window Back = new MainWindow();
+            Back.Show();
+            Close();
+        }
+    }
+}
